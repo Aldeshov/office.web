91031630bfea1066bfff154084c392e30ff5139d
Aldeshov <47108137+Aldeshov@users.noreply.github.com>
2020-04-21 17:13:19 +0600

    Update settings.py

--- officeRegistrator-api/office/settings.py
+++ officeRegistrator-api/office/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&*vyc^i@bwr_02dp!+)ra3lky811l+i@92l@4)%@n9n3x^t3zz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
