d76e350e46e83555a28017f70a23ff8278e38efc
Aldeshov <azat.aldeshov@gmail.com>
2020-04-26 21:28:09 +0600

    Project

--- officeRegistrator-api/office/settings.py
+++ officeRegistrator-api/office/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'office',
-        'USER': 'postgre',
+        'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '',
