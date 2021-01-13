4eb31e1bbbf3db95b6ea56fb3d0474cba22558df
Aldeshov <azat.aldeshov@gmail.com>
2021-01-13 14:19:01 +0600

    office

--- officeRegistrator-api/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Course, Student, Teacher, News, File
-
-admin.site.register(Course)
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(News)
-admin.site.register(File)
