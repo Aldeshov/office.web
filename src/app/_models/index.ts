2e639728ab10aab7936735224406137e56fa2b6c
Aldeshov <azat.aldeshov@gmail.com>
2021-01-13 20:03:00 +0600

    office

--- src/app/_models/index.ts
+++ src/app/_models/index.ts
@@ -3,5 +3,5 @@ export * from './Teacher';
 export * from './Student';
 export * from './Course';
 export * from './News';
-export * from './Coursefile';
+export * from './CourseFile';
 export * from './login';
